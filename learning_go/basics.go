496297c8cabcb37538a90c2aa423158fd98e6f4e
JuleeC <juleshummel44@gmail.com>
2025-06-21 13:15:42 +0000

    basics

--- learning_go/basics.go
+++ learning_go/basics.go
@@ -6,7 +6,16 @@ package main
 
 import "fmt"
 
+// declaring variables
+var a int
+var b bool
+var c string
+//it is not necessary to declare a type
 func main()  {
 	fmt.Printf("Hello World")
 	fmt.Printf("testing if commiting works")
+	//you can do this also but only in a function
+	a1 := 10
+	b2 := true
+	c3 := "hello"
 }
