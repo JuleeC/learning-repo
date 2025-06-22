bd13ac1b26946c918e4f09d163584b8c88d1add3
JuleeC <juleshummel44@gmail.com>
2025-06-22 20:40:21 +0000

    lists

--- learning_go/basics.go
+++ learning_go/basics.go
@@ -66,6 +66,11 @@ func main()  {
 			}
 			// continue is used to skip the rest of the loop and works also with labels
 
+			// range is used to iterate over arrays and slices
+			list := []char{'a', 'b', 'c'}
+			for k, v := range list {
+				fmt.Printf("k is %d and v is %c\n", k, v)
+			}
 }
 
 //goto is used to jump to a label
