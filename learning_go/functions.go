36c605b139c0e87932ac04951f56d12f260f6ee2
JuleeC <juleshummel44@gmail.com>
2025-06-27 12:30:56 +0000

    Scopes#

--- learning_go/functions.go
+++ learning_go/functions.go
@@ -6,6 +6,12 @@ import (
 //functions are declared with func
 //syntax is : func(p type)name(parameters) return_type {}
 // functions can be declared in any order because the compiler executes after scanning the whole file
+
+//scope is the region of code that a variable is valid
+// vars that are declared outisde of a function are global
+// vars that are declared inside a function are local
+
+
 func main() {
 	fmt.Println("Hello World!")
 }
@@ -13,7 +19,7 @@ func rec(n int) int {
 	if n == 0 {
 		return 1
 	}
+	//recursion
 	rec(n - 1)
 	return 0
-
 }
