26f8c5d193919cc6f0ad0487c21060825d4ffe86
JuleeC <juleshummel44@gmail.com>
2025-06-27 12:28:16 +0000

    comp

--- learning_go/functions.go
+++ learning_go/functions.go
@@ -5,7 +5,7 @@ import (
 )
 //functions are declared with func
 //syntax is : func(p type)name(parameters) return_type {}
-
+// functions can be declared in any order because the compiler executes after scanning the whole file
 func main() {
 	fmt.Println("Hello World!")
 }
