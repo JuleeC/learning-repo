e5ce5e691468bd499b498299aff547c55cb8c23c
JuleeC <juleshummel44@gmail.com>
2025-06-30 12:29:47 +0000

    global local

--- learning_go/package/main.go
+++ learning_go/package/main.go
@@ -6,9 +6,12 @@ import (
 	"package/packages/even"
 )
 
+// capital letter = gloabl scope
+// small letter = local scope
+// underscore = local scope
 func main() {
 	fmt.Println("packages")
 	fmt.Println(even.Even(2))
 	// this doesnt work because odd is not in the packages scope
-	fmt.Println(even.odd(2))
+	// fmt.Println(even.odd(2))
 }
