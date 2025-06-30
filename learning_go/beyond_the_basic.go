ac962712bffcf70633b0a5b7de09b26df37819c9
JuleeC <juleshummel44@gmail.com>
2025-06-30 18:32:57 +0000

    beyond the basics§

--- learning_go/beyond_the_basic.go
+++ learning_go/beyond_the_basic.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// go pointers isnt arithmetic like in c, its more of an reference
+// when passing a pointer to a function, the function can modify the value
 func main() {
 	fmt.Println("beyond the basics")
 }
