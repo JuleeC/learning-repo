e0c730d7b533309d570ace605454fd15bbd274e4
JuleeC <juleshummel44@gmail.com>
2025-07-01 12:07:47 +0000

    .

--- learning_go/beyond_the_basic.go
+++ learning_go/beyond_the_basic.go
@@ -6,6 +6,10 @@ import (
 
 // go pointers isnt arithmetic like in c, its more of an reference
 // when passing a pointer to a function, the function can modify the value
+// assigning a pointer to a variable without a value ist called nil pointer
+// go has a garbage collector that frees up memory that is no longer in use
+// but you can also manually free up memory with the free function or make  function
+// sometimes zero values isnt good enough so initializing a constructor is a good idea
 func main() {
 	fmt.Println("beyond the basics")
 }
