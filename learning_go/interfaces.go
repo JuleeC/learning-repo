8fac74b720a904677d0de2a99645b28acf47eb8e
JuleeC <juleshummel44@gmail.com>
2025-07-03 12:21:32 +0000

    types for interfaces

--- learning_go/interfaces.go
+++ learning_go/interfaces.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// in other words, interfaces has overloaded things that it means
+// a interface has a set of methods that is defined by a type
 func main() {
 	fmt.Println("interfaces")
 }
