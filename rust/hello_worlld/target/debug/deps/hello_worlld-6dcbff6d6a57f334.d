f5d30c246f0f0e0e63fe98a2604ba86d8614945a
JuleeC <juleshummel44@gmail.com>
2025-07-12 14:21:14 +0000

    finally a real project

---
+++ rust/hello_worlld/target/debug/deps/hello_worlld-6dcbff6d6a57f334.d
@@ -0,0 +1,5 @@
+/home/julee/dev/learning-repo/rust/hello_worlld/target/debug/deps/hello_worlld-6dcbff6d6a57f334.d: src/main.rs
+
+/home/julee/dev/learning-repo/rust/hello_worlld/target/debug/deps/libhello_worlld-6dcbff6d6a57f334.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_worlld/target/debug/deps/hello_worlld-f40a45727a17aaa2.d
@@ -0,0 +1,5 @@
+/home/julee/dev/learning-repo/rust/hello_worlld/target/debug/deps/hello_worlld-f40a45727a17aaa2.d: src/main.rs
+
+/home/julee/dev/learning-repo/rust/hello_worlld/target/debug/deps/libhello_worlld-f40a45727a17aaa2.rmeta: src/main.rs
+
+src/main.rs:
