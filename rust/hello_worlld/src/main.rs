35f45400e513b659d307d98af0be41a3aee1eaae
JuleeC <juleshummel44@gmail.com>
2025-07-13 13:34:41 +0000

    formatting

--- rust/hello_worlld/src/main.rs
+++ rust/hello_worlld/src/main.rs
@@ -179,4 +179,44 @@ fn formatting() {
         green: u8,
         blue: u8,
     }
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ] {
+        println!("{}", city);
+    }
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ] {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", color);
+    }
 }
