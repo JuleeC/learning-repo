642267f71cf2069afec1bcc3b33dd48c8409e1af
JuleeC <juleshummel44@gmail.com>
2025-07-14 13:01:56 +0000

    tuples

--- rust/primitives/src/main.rs
+++ rust/primitives/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     primitves();
     literals_and_operators();
+    tuples();
 }
 fn primitves() {
     println!("primitves");
@@ -52,6 +53,9 @@ fn primitves() {
     let my_tuple = (5u32, 1u8, true, -5.04f32);
 }
 fn literals_and_operators() {
+    //when declaring a varibale using underscores is also allowed to improve readability
+    let x = 1_000_000;
+    println!("x is {}", x);
     // Integer addition
     println!("1 + 2 = {}", 1u32 + 2);
 
@@ -75,4 +79,50 @@ fn literals_and_operators() {
 
     // Use underscores to improve readability!
     println!("One million is written as {}", 1_000_000u32);
+}
+fn tuples() {
+    // Tuples can be used as function arguments and as return values.
+
+    // The following struct is for the activity.
+    #[derive(Debug)]
+    struct Matrix(f32, f32, f32, f32);
+
+    // A tuple with a bunch of different types.
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    // Values can be extracted from the tuple using tuple indexing.
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members.
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable.
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // But long Tuples (more than 12 elements) cannot be printed.
+    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    //println!("Too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    println!("The reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses.
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+
+    // Tuples can be destructured to create bindings.
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
 }
